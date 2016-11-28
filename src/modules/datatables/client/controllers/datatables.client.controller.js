12de3d80a55f0adfc7e0cbe68068f5eb80f221e5
Rubén Brieva Rodríguez <rubenbrieva@gmail.com>
2016-11-28 11:01:51 +0100

    Update modules base

--- src/modules/datatables/client/controllers/datatables.client.controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('datatables')
-  .controller('DatatablesController', ['$scope', 'title', ($scope, title) => {
-    $scope.page = {
-      title: title.value,
-    };
-  }]);
--- src/modules/datatables/server/routes/datatables.server.routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-  const datatablesController = require('../controllers/datatables.server.controller')(app);
-
-  app.route('/api/list/:entity')
-    .get(datatablesController.list);
-};
