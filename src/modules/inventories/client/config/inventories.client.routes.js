62d68acf427194c253fd3089062b296ec7291d0c
Daniel Martínez <damarna85@gmail.com>
2016-11-29 16:53:08 +0100

    Include product detail services

--- src/modules/inventories/client/config/inventories.client.routes.js
+++ src/modules/inventories/client/config/inventories.client.routes.js
@@ -4,6 +4,10 @@ angular.module('inventories').config(['$stateProvider',
       .state('inventories', {
         url: '/inventories',
         templateUrl: 'views/inventories.client.view.html',
+      })
+      .state('product-detail', {
+        url: '/inventories/product/:productId/:sku',
+        templateUrl: 'views/inventories-detail.client.view.html',
       });
   },
 ]);
