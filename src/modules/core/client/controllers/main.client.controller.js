4f5f3ca713548433746ee92024f837def69481bf
Rubén Brieva Rodríguez <rubenbrieva@gmail.com>
2016-11-29 18:12:11 +0100

    Remove Sample Data Remove Inventory Controller, changed by paginator module Used custom table

--- src/modules/core/client/controllers/main.client.controller.js
+++ src/modules/core/client/controllers/main.client.controller.js
@@ -1,23 +1,6 @@
 angular.module('core')
-  .controller('MainCtrl', function ($scope, $http, $translate) {
-
+  .controller('MainCtrl', ($scope) => {
     $scope.main = {
-      title: 'Commercetools'
+      title: 'Commercetools',
     };
-
-    $scope.ajaxFaker = function(){
-      $scope.data=[];
-      var url = 'http://www.filltext.com/?rows=10&fname={firstName}&lname={lastName}&delay=5&callback=JSON_CALLBACK';
-
-      $http.jsonp(url).success(function(data){
-        $scope.data=data;
-        angular.element('.tile.refreshing').removeClass('refreshing');
-      });
-    };
-
-    $scope.changeLanguage = function (langKey) {
-      $translate.use(langKey);
-      $scope.currentLanguage = langKey;
-    };
-    $scope.currentLanguage = $translate.proposedLanguage() || $translate.use();
   });
---
+++ src/modules/paginator/client/controllers/paginator.client.controller.js
@@ -0,0 +1,200 @@
+angular.module('paginator')
+  .controller('PaginatorController', ['$scope', '$http', 'ChannelSelector',
+  ($scope, $http, ChannelSelector) => {
+    $scope.itemsPerPage = 25;
+
+    function getMatchFields(fields, key) {
+      let matchFields;
+
+      if (fields && fields.length > 0) {
+        matchFields = [];
+
+        fields.forEach((field) => {
+          const arr = field.split('.');
+          if (arr[0] === key) {
+            arr.splice(0, 1);
+
+            if (arr.length > 0) {
+              matchFields.push(arr.join('.'));
+            }
+          }
+        });
+      }
+
+      return matchFields;
+    }
+
+    function keyMatch(fields, key) {
+      let result = false;
+
+      if (fields && fields.length > 0) {
+        fields.forEach((field) => {
+          if (field === key) {
+            result = true;
+          }
+        });
+      }
+
+      return result;
+    }
+
+    function getFilter(predicate, composeFields, noCaseSensitiveFields, lowercaseFields) {
+      const filter = {};
+
+      Object.keys(predicate).forEach((key) => {
+        if (predicate.hasOwnProperty(key)) {
+          let noCaseSensitiveMatchedFields = null;
+          let lowercaseMatchedFields = null;
+
+          if (typeof predicate[key] === 'object') {
+            noCaseSensitiveMatchedFields = getMatchFields(noCaseSensitiveFields, key);
+            lowercaseMatchedFields = getMatchFields(lowercaseFields, key);
+
+            filter[key] = getFilter(predicate[key], composeFields,
+                          noCaseSensitiveMatchedFields, lowercaseMatchedFields);
+          } else {
+            const isNoCaseSensitive = keyMatch(noCaseSensitiveFields, key);
+            const isLowercase = keyMatch(lowercaseFields, key);
+
+            filter[key] = {
+              value: predicate[key].trim(),
+              noCaseSensitive: isNoCaseSensitive,
+              lowercase: isLowercase,
+            };
+          }
+        }
+      });
+
+      return filter;
+    }
+
+
+    $scope.getPage = (tableState) => {
+      const pagination = tableState.pagination;
+      const sort = tableState.sort;
+      const search = tableState.search;
+
+      // This is NOT the page number, but the index of item in the list that you
+      // want to use to display the table.
+      $scope.start = pagination.start || 0;
+      // Number of entries showed per page.
+      $scope.perPage = pagination.number || $scope.itemsPerPage;
+      $scope.page = Math.floor($scope.start / $scope.perPage) + 1;
+      $scope.sortBy = sort.predicate || $scope.paginatorDefaultSort || 'createdAt';
+      $scope.sortAscending = typeof sort.reverse === 'undefined' ? false : !sort.reverse;
+      $scope.sortPredicateObject = sort;
+      $scope.predicateObject = search && search.predicateObject;
+      $scope.filter = search && search.predicateObject ?
+        getFilter(search.predicateObject, $scope.composeFields, $scope.noCaseSensitveFields,
+                  $scope.lowercaseFields) : '';
+
+      $http.get(`/api/${$scope.entity}`, {
+        params: {
+          page: $scope.page,
+          perPage: $scope.perPage,
+          selectedChannel: ChannelSelector.selectedChannel,
+          filter: JSON.stringify($scope.filter),
+          sortBy: $scope.sortBy,
+          sortAscending: $scope.sortAscending,
+          expandPaths: JSON.stringify($scope.expandPaths),
+        },
+      })
+      .then((response) => {
+        tableState.pagination.totalItemCount = (response.data.total) ? response.data.total : response.data.length;
+        tableState.pagination.numberOfPages = Math.ceil(response.data.total / $scope.perPage);
+        tableState.pagination.start = $scope.start;
+        tableState.search.predicateObject = $scope.predicateObject;
+        tableState.sort = $scope.sortPredicateObject;
+        $scope.rowsDisplayed = (response.data.results) ? response.data.results : response.data;
+      })
+      .catch((err) => {
+        $scope.rowsDisplayed = [];
+        console.log('Error: %s', err);
+      });
+    };
+
+    $scope.setItemsPerPage = (value) => {
+      $scope.itemsPerPage = value;
+    };
+  }])
+  .directive('paginatorEntity', () => {
+    return {
+      restrict: 'A',
+      controller: 'PaginatorController',
+      link: (scope, element, attrs) => {
+        scope.entity = attrs.paginatorEntity;
+      },
+    };
+  })
+  .directive('paginatorDefaultSort', () => {
+    return {
+      restrict: 'A',
+      controller: 'PaginatorController',
+      link: (scope, element, attrs) => {
+        scope.paginatorDefaultSort = attrs.paginatorDefaultSort;
+      },
+    };
+  })
+  .directive('pageSelect', () => {
+    return {
+      restrict: 'E',
+      template: `<input type="text" class="select-page col-md-5" ng-model="inputPage"
+                    ng-change="selectPage(inputPage)">`,
+      link: (scope) => {
+        scope.$watch('currentPage', (c) => {
+          scope.inputPage = c;
+        });
+      },
+    };
+  })
+  .directive('lowerCase', () => {
+    return {
+      restrict: 'A',
+      controller: 'PaginatorController',
+      link: (scope, element, attrs) => {
+        if (typeof attrs.lowerCase !== 'undefined' && attrs.stSearch) {
+          scope.lowercaseFields = scope.lowercaseFields || [];
+          scope.lowercaseFields.push(attrs.stSearch);
+        }
+      },
+    };
+  })
+  .directive('noCaseSensitive', () => {
+    return {
+      restrict: 'A',
+      controller: 'PaginatorController',
+      link: (scope, element, attrs) => {
+        if (typeof attrs.noCaseSensitive !== 'undefined' && attrs.stSearch) {
+          scope.noCaseSensitveFields = scope.noCaseSensitveFields || [];
+          scope.noCaseSensitveFields.push(attrs.stSearch);
+        }
+      },
+    };
+  })
+  .directive('expandPaths', () => {
+    return {
+      restrict: 'A',
+      controller: 'PaginatorController',
+      link: (scope, element, attrs) => {
+        if (attrs.expandPaths) {
+          scope.expandPaths = [];
+
+          const fields = attrs.expandPaths.split(',');
+
+          fields.forEach((field) => {
+            scope.expandPaths.push(field.trim());
+          });
+        }
+      },
+    };
+  })
+  .filter('imageUrlFilter', () => {
+    return (input, size) => {
+      if (input) {
+        const regex = /^(.+)\.([0-9a-z]+)$/i;
+        const parts = input.match(regex);
+
+        return `${parts[1]}-${size}.${parts[2]}`;
+      }
+    };
+  });
